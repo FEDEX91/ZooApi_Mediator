2f10c71f82580ebb9bd2f86b048af615b296fff1
Federico Martinez <federicomtz38@hotmail.com>
2025-08-08 13:43:15 -0600

    Clean handler

--- ZooApi_Mediator/Application/Features/Bird/Commands/CreateBirdCommandHandler.cs
+++ ZooApi_Mediator/Application/Features/Bird/Commands/CreateBirdCommandHandler.cs
@@ -7,17 +7,14 @@ namespace ZooApi_Mediator.Application.Features.Bird.Commands
 {
     public class CreateBirdCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateBirdCommand, BirdDto>
     {
-        private readonly IUnitOfWork _unitOfWork = unitOfWork;
-        private readonly IMapper _mapper = mapper;
-
         public async Task<BirdDto> Handle(CreateBirdCommand request, CancellationToken cancellationToken)
         {
-            var bird = this._mapper.Map<Domain.Entities.Bird>(request.BirdDto);
+            var bird = mapper.Map<Domain.Entities.Bird>(request.BirdDto);
 
-            await _unitOfWork.Birds.AddAsync(bird);
-            await _unitOfWork.SaveChangesAsync();
+            await unitOfWork.Birds.AddAsync(bird);
+            await unitOfWork.SaveChangesAsync();
 
-            return this._mapper.Map<BirdDto>(bird);
+            return mapper.Map<BirdDto>(bird);
         }
     }
 }
